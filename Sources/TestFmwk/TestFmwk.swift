5f1e0f2f5310911cf6d7582a599534783c9f9df6
Thomas <thomas@dougnac.fr>
2018-01-24 15:34:21 +0100

    MyStruct

--- Sources/TestFmwk/TestFmwk.swift
+++ Sources/TestFmwk/TestFmwk.swift
@@ -1,3 +1,29 @@
-struct TestFmwk {
-    var text = "Hello, World!"
+import Foundation
+
+public struct MyStruct {
+    
+    public var type: MyTypeEnum
+    public var item: MyItem
+    
+    public enum MyTypeEnum {
+        case a
+        case b
+        case utility
+    }
+    
+    public init(type: MyTypeEnum, itemType: MyItemTypeEnum, data: Data? = nil) {
+        self.type = type
+        self.item = MyItem(type: itemType, data: data)
+    }
+    
+    public struct MyItem {
+        public var type: MyItemTypeEnum
+        public var data: Data?
+    }
+    
+    public enum MyItemTypeEnum {
+        case itemA
+        case itemB
+        case utility
+    }
 }
